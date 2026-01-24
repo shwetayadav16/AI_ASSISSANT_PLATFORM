b76c23eb617c1ce521da243485e3ae21af0f10c5
Shweta Yadav <shwetayadav3490@gmail.com>
2026-01-25 00:00:29 +0530

    controller->register setup

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -11,7 +11,43 @@ const generateToken=(id)=>{
 //@access Public    
 export const register=async(req,res,next)=>{
     try{
+        const{username,email,password}=req.body;
+        //Check if user exists
+        const userExists=await User.findOne({$or:[{email}] });
+        if(userExists){
+            return res.status(400).json({
+                success:false,
 
+                error:
+                userExists.email===email?
+                "Email already registered"
+                :"Username already taken",
+                statusCode:400
+            });
+        }
+        //Create user
+        const user=await User.create({
+            username,
+            email,
+            password,
+        });
+        //Generated token
+        const token= generateToken(user._id);
+
+        res.status(201).json({
+            success:true,
+            data:{
+                user:{
+                    id:user._id,
+                    username:user.username,
+                    email:user.email,
+                    profileImage:user.profileImage,
+                    createdAt:user.createdAt,
+                },
+                token,
+            },
+            message:"User registered successfully",
+        });
     }
     catch(error){
         next(error);
