6f990e110d88b43cfe2b3564f6fde2e40fba307c
Shweta Yadav <shwetayadav3490@gmail.com>
2026-02-02 20:52:37 +0530

    models folder setup

---
+++ backend/models/Flashcard.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+const flashcardSchema = new mongoose.Schema(
+    {
+        userId:{
+            type:mongoos.Schema.Types.ObjectId,
+            ref:"User",
+            required:true
+        },
+        documentId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Document",
+            required:true,
+        },
+        cards:[
+            {
+                questions:{type:String,required:true},
+                answer:{ type:String,required:true},
+                difficulty:{
+                    type:String,
+                    enum:["easy","medium","hard"],
+                    default:"medium",
+                },
+                isStarred:{
+                    type:Boolean,
+                    default:false,
+                },
+
+            },
+        ],
+    },
+{
+   timestamps:true,
+})
+flashcardSchema.index({userId:1,documentId:1});
+const Flashcard=mongoose.model("Flashcard",flashcardSchema);
+export default Flashcard;
